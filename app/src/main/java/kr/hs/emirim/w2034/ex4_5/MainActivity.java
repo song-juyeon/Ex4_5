ba4f89f3451bbbd0f55770b6affee8dcd7a9f6c7
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2021-07-26 13:29:13 +0900

    Edit MainActivity.java(Add Item) Commit

--- app/src/main/java/kr/hs/emirim/w2034/ex4_5/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2034/ex4_5/MainActivity.java
@@ -3,12 +3,35 @@ package kr.hs.emirim.w2034.ex4_5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> arrList;
+    ArrayAdapter<String> adapter;
+    EditText editItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        arrList = new ArrayList<>();
+        ListView list1 = findViewById(R.id.list1);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrList);
+        list1.setAdapter(adapter);
+        Button btnAdd = findViewById(R.id.btn_add);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrList.add(editItem.getText().toString());
+                adapter.notifyDataSetChanged();     //새로고침(?)
+            }
+        });
+
     }
 }
